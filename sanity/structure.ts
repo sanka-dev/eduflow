8c5095b7e591c66f5f0440972e47a4a65275e070
sanka-x <chsshansanath@students.nsbm.ac.lk>
2025-08-10 08:16:21 +0530

    Add stegaEncodeSourceMap functionality

--- sanity/structure.ts
+++ sanity/structure.ts
@@ -1,6 +1,5 @@
 import { StructureBuilder } from "sanity/structure";
 
-// https://www.sanity.io/docs/structure-builder-cheat-sheet
 export const structure = (S: StructureBuilder) =>
   S.list()
     .title("Admin Dashboard")
